e98439c89899be46cc1c07001a60fbb20205bb59
iamCHO <iamcho@qq.com>
2024-04-24 21:56:24 +0800

    Update auto8.sh

--- auto8.sh
+++ auto8.sh
@@ -39,5 +39,4 @@ echo "-------------------------"
 echo "    MINING  XENBLOCKS    "
 echo "     https://xen.pub     "
 echo "-------------------------"
-echo " "
-tail -f /root/XENGPUMiner/miner.log
+
