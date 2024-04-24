9a352b190d3459c04f713ae4cf44fdd2cc2198e4
iamCHO <iamcho@qq.com>
2024-04-25 00:32:58 +0800

    Update auto8.sh

--- auto8.sh
+++ auto8.sh
@@ -14,6 +14,10 @@ echo "STEP 7 of 10: Permissions set!"
 sudo sed -i "s/account = $1/account = $2/g" config.conf > /dev/null 2>&1
 echo "-----auto8.sh start------：$2"
 
+# 杀掉之前的xengpuminer进程
+sudo killall xengpuminer
+echo "-----kill xengpuminer-----------"
+
 sudo nohup python3 miner.py --gpu=true > miner.log 2>&1 &
 sleep 1
 sudo nohup ./xengpuminer -d0 > xengpuminer-0.log 2>&1 &
