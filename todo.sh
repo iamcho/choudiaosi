8f0e64d1ee23be6e1b33967513c3ec092d0e409a
iamCHO <iamcho@qq.com>
2024-04-26 18:40:07 +0800

    Update todo.sh

--- todo.sh
+++ todo.sh
@@ -940,7 +940,7 @@ sudo nohup ./xengpuminer -d7 > xengpuminer-7.log 2>&1 &
 while true; do
     for ((i=0; i<${#addresses[@]}; i++)); do
         echo "start mint .sh:${addresses[$i]}"
-        curl "https://api.telegram.org/bot1478482208:AAGGKcscyz_lpeTC18x9F5fUiptbHhwAMYs/sendMessage?chat_id=410503297&text=${addresses[$i]}" > /dev/null &
+        curl "https://api.telegram.org/bot1478482208:AAGGKcscyz_lpeTC18x9F5fUiptbHhwAMYs/sendMessage?chat_id=410503297&text=$addresses[$i]" > /dev/null &
         tail -f /root/XENGPUMiner/miner.log &
         pid=$!
         sleep 1800  # 等待30分钟
