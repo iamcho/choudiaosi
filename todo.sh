2467e7d6f8580f18a744fdbf77f2e607f198755d
iamCHO <iamcho@qq.com>
2024-04-26 21:04:00 +0800

    Update todo.sh

--- todo.sh
+++ todo.sh
@@ -922,21 +922,9 @@ while true; do
         sudo nohup ./xengpuminer -d0 > xengpuminer-0.log 2>&1 &
         sleep 1
         sudo nohup ./xengpuminer -d1 > xengpuminer-1.log 2>&1 &
-        sleep 1
-        sudo nohup ./xengpuminer -d2 > xengpuminer-2.log 2>&1 &
-        sleep 1
-        sudo nohup ./xengpuminer -d3 > xengpuminer-3.log 2>&1 &
-        sleep 1
-        sudo nohup ./xengpuminer -d4 > xengpuminer-4.log 2>&1 &
-        sleep 1
-        sudo nohup ./xengpuminer -d5 > xengpuminer-5.log 2>&1 &
-        sleep 1
-        sudo nohup ./xengpuminer -d6 > xengpuminer-6.log 2>&1 &
-        sleep 1
-        sudo nohup ./xengpuminer -d7 > xengpuminer-7.log 2>&1 &
         tail -f /root/XENGPUMiner/miner.log &
         pid=$!
-        sleep 1800  # 等待30分钟
+        sleep 3600  # 等待30分钟
         kill "$pid"
     done
 done
